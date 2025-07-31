898d3b971b8c54ccc3f69525d514ac3b4421bb2b
Md Danish Raza <md.danish0raza@gmail.com>
2025-07-31 21:39:25 +0530

    zod validation schema

--- utils/actions.ts
+++ utils/actions.ts
@@ -1,7 +1,9 @@
 "use server";
 
 import db from "@/utils/db";
+import { currentUser } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
+import { productSchema } from "./validationSchemas";
 export async function fetchFeaturedProducts() {
   const products = await db.product.findMany({
     where: {
@@ -43,12 +45,49 @@ export const fetchSingleProduct = async (productId: string) => {
   return product;
 };
 
+// helper fns
+const renderError = (error: unknown): { message: string } => {
+  console.log(error);
+  return {
+    message: error instanceof Error ? error.message : "An error occurred",
+  };
+};
+
+const getAuthUser = async () => {
+  const user = await currentUser();
+  if (!user) {
+    throw new Error("You must be logged in to access this route");
+  }
+  return user;
+};
+
 export const createProductAction = async (
   prevState: any,
   formData: FormData
 ): Promise<{ message: string }> => {
-  "use server";
-  const form = Object.fromEntries(formData.entries());
-  console.log(form);
-  return { message: "product created" };
+  const user = await getAuthUser();
+
+  try {
+    const rawData = Object.fromEntries(formData);
+    // safe parse for custom error messages gracefully
+    const validatedData = productSchema.safeParse(rawData);
+    // if not valid inputs
+    if (!validatedData.success) {
+      // getting all errors messages of array
+      const errors = validatedData.error.issues.map((issue) => issue.message);
+      // returning into a combined string message
+      throw new Error(errors.join(","));
+    }
+
+    // await db.product.create({
+    //   data: {
+    //     ...validatedData.data,
+    //     image: "/images/test.png",
+    //     clerkId: user.id,
+    //   },
+    // });
+    return { message: "product created" };
+  } catch (error) {
+    return renderError(error);
+  }
 };
