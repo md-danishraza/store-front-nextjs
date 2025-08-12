fddbe077c2f30e0156a82508ef7aafda8b6cca7b
Md Danish Raza <md.danish0raza@gmail.com>
2025-08-12 18:42:17 +0530

    sales and orders before payment integration

--- utils/format.ts
+++ utils/format.ts
@@ -5,3 +5,11 @@ export const formatCurrency = (amount: number | null) => {
     currency: "INR",
   }).format(value);
 };
+
+export const formatDate = (date: Date) => {
+  return new Intl.DateTimeFormat("en-IN", {
+    day: "2-digit",
+    month: "long",
+    year: "numeric",
+  }).format(date);
+};
